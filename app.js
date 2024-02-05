e2cdd030750bd9416c2cd0167739d88101db9551
Shivasai-k <142988677+Shivasai-k@users.noreply.github.com>
2024-02-05 23:42:15 +0530

    Testcase failed for API1 (/?status=TO%20DO)

--- app.js
+++ app.js
@@ -4,19 +4,20 @@ const sqlite3 = require("sqlite3");
 const path = require("path");
 
 const app = express();
+app.use(express.json());
 
 let db = null;
 
-const dbpath = path.join(__dirname, "todoApplication.db");
+const dbPath = path.join(__dirname, "todoApplication.db");
 
 const initializeDBAndServer = async () => {
   try {
     db = await open({
-      filename: dbpath,
+      filename: dbPath,
       driver: sqlite3.Database,
     });
-    app.listen(3001, () => {
-      console.log("Server is running at http://localhost:3001/");
+    app.listen(3000, () => {
+      console.log("Server is running at http://localhost:3000/");
     });
   } catch (e) {
     console.log(`DB Error : ${e.message}`);
@@ -90,3 +91,68 @@ app.get("/todos/", async (request, response) => {
   data = await db.all(getTodosQuery);
   response.send(data);
 });
+
+//API 2
+app.get("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  const getTodoQuery = `
+        select * from todo where id=${todoId};
+    `;
+  const todo = await db.get(getTodoQuery);
+  response.send(todo);
+});
+
+//API 3
+app.post("/todos/", async (request, response) => {
+  const todoDetails = request.body;
+  const { id, todo, priority, status } = todoDetails;
+  const postTodoQuery = `
+        insert into todo(id,todo,priority,status) values(${id},'${todo}','${priority}','${status}');
+    `;
+  await db.run(postTodoQuery);
+  response.send("Todo Successfully Added");
+});
+
+//API 4
+app.put("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  let putTodoQuery = "";
+  const todoDetails = request.body;
+  const { status, priority, todo } = todoDetails;
+  let updateResponse = "";
+  switch (true) {
+    case hasStatusProperty(todoDetails):
+      putTodoQuery = `
+                    update todo set status='${status}' where id=${todoId};
+                `;
+      updateResponse = "Status Updated";
+      break;
+
+    case hasPriorityProperty(todoDetails):
+      putTodoQuery = `
+                    update todo set priority='${priority}' where id=${todoId}
+                `;
+      updateResponse = "Priority Updated";
+      break;
+    default:
+      putTodoQuery = `
+                    update todo set todo='${todo}' where id=${todoId}
+                `;
+      updateResponse = "Todo Updated";
+      break;
+  }
+  await db.run(putTodoQuery);
+  response.send(updateResponse);
+});
+
+//API 5
+app.delete("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  const deleteTodoQuery = `
+        delete from todo where id=${todoId};
+    `;
+  await db.run(deleteTodoQuery);
+  response.send("Todo Deleted");
+});
+
+module.exports = app;
